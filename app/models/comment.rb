674e6fad2f8b642e4ed19a9f0b6980b4a2abfa5c
CJ517B <cj517b@users.noreply.github.com>
2016-11-21 15:36:02 -0600

    11.21.2016 15:35pm

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ActiveRecord::Base
+
+  belongs_to :user, :class_name => "User", :foreign_key => "user_id"
+
+  belongs_to :photo, :class_name => "Photo", :foreign_key => "photo_id"
+
+end
---
+++ app/models/photo.rb
@@ -0,0 +1,6 @@
+class Photo < ActiveRecord::Base
+  belongs_to :user, :class_name => "User", :foreign_key => "photo_id"
+  has_many :comments, :class_name => "Comment", :foreign_key => "photo_id"
+  has_many :likes, :class_name => "Like", :foreign_key => "photo_id"
+  has_many :fans, :through => :likes, :source => :user
+end
